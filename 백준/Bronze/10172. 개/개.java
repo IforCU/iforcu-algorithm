08e8592b93c9a78eaa026df9c5bedef172a6122f
I4CU <129958775+IforCU@users.noreply.github.com>
2026-02-27 17:29:57 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/| \n").append("|q p|   /} \n").append("( 0 )\"\"\"\\ \n").append("|\"^\"`    | \n").append("||_/=\\\\__| ");
+        
+        System.out.print(sb);
+    }
+}
