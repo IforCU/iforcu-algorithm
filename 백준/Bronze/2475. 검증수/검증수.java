d7d14effd19feb89bd1f607766c608a8f41b9419
I4CU <129958775+IforCU@users.noreply.github.com>
2026-02-27 17:09:58 +0900

    [Bronze V] Title: 검증수, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int ans = 0;
+        for(int i = 0 ; i < 5; i++) {
+            ans += Math.pow(Float.parseFloat(st.nextToken()), 2);
+        }
+        System.out.println(ans%10);
+    }
+}
