9532137a32822337e5b331d5f89b7e8042d51df2
I4CU <129958775+IforCU@users.noreply.github.com>
2026-02-27 17:28:52 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13840 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\ \n").append(" )  ( ') \n").append("(  /  ) \n").append(" \\(__)|");
+        
+        System.out.print(sb);
+    }
+}
